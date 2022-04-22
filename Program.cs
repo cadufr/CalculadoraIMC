dd858e848c383c933cfcb5a3d926656f77fc7c9e
Cadu :) <102222757+cadufr@users.noreply.github.com>
2022-04-22 13:04:36 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace CalculadoraIMC
 {
@@ -6,54 +6,20 @@ namespace CalculadoraIMC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Entre com seu nome");
+            Console.WriteLine("Entre com o seu nome");
             string nome = Console.ReadLine();
 
-            Console.WriteLine("Entre com sua altura");
+            Console.WriteLine("Entre com sua altura em metros. Ex: 1,65");
             float altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Entre com o seu peso. Ex: 67,7");
+            Console.WriteLine("Entre com o seu peso em Kg. Ex: 67,7");
             float peso = Convert.ToSingle(Console.ReadLine());
 
-            double imc = peso / (altura * altura);
+            Individuo i1 = new Individuo(nome, peso, altura);
 
-            Console.WriteLine("{0} sua altura é  {1} e seu peso é {2}", nome, altura , peso);
-            Console.WriteLine("Seu IMC é {0}", imc);
-
-            if(imc < 17.00)
-            {
-                Console.WriteLine("Muito abaixo do peso normal");
-            }
-
-            else if(imc >= 17.00 && imc < 18.50)
-            {
-                Console.WriteLine("Abaixo do peso normal");
-            }
-
-            else if(imc >= 18.50 && imc < 25.00)
-            {
-                Console.WriteLine("Peso normal");
-            }
-
-            else if(imc >=  25.00 && imc < 30.00)
-            {
-                Console.WriteLine("Sobrepeso");
-            }
-
-            else if(imc >= 30.00 && imc < 35.00)
-            {
-                Console.WriteLine("Obesidade grau I");
-            }
-
-            else if(imc >= 35.00 && imc < 40.00)
-            {
-                Console.WriteLine("Obesidade grau II");
-            }
-
-            else if(imc >= 40.00 && imc < 45.00)
-            {
-                Console.WriteLine("Obesidade grau III");
-            }
+            Console.WriteLine(i1.IMC);
+            Console.WriteLine(i1.ClassificacaoIMC());
+            Console.WriteLine(i1.ParaPesoIdeal());
 
         }
     }
